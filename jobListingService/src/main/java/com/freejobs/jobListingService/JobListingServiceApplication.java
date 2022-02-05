7d864a48aa6fc7d316afdfa557623db860bdd1e7
Yvoone <yvoone97@gmail.com>
2022-02-05 16:35:48 +0800

    First Commit (jobListingService)

---
+++ jobListingService/src/main/java/com/freejobs/jobListingService/JobListingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.freejobs.jobListingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobListingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobListingServiceApplication.class, args);
+	}
+
+}
---
+++ jobListingService/src/test/java/com/freejobs/jobListingService/JobListingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freejobs.jobListingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobListingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
