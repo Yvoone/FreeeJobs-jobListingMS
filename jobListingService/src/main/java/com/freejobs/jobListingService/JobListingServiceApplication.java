de11eee8327b46100924baa3669b5f7e90537fc9
Sydney Gunady (NCS) <sydney.gunady@ncs.com.sg>
2022-02-05 20:32:04 +0800

    test push

--- jobListingService/src/main/java/com/freejobs/jobListingService/JobListingServiceApplication.java
+++ jobListingService/src/main/java/com/freejobs/jobListingService/JobListingServiceApplication.java
@@ -3,6 +3,7 @@ package com.freejobs.jobListingService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//jobListingService
 @SpringBootApplication
 public class JobListingServiceApplication {
 
